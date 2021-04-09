90bae02df97daaf455b70db04d8a178f5c0e01f3
duy le <duy.leduc8@gmail.com>
2021-04-09 17:16:33 +0700

    9/4/2021

--- shuffle_arr.py
+++ shuffle_arr.py
@@ -5,8 +5,25 @@ from typing import List
 
 def shuffle(nums: List[int], n: int) -> List[int]:
     res = []
-    
+
+    for i, num in enumerate(nums):
+        if(i == n or i + n == len(nums) - 1):
+            break
+        res.append(nums[i])
+        res.append(nums[i + n])
+        i += 1
+
+    while i <= n:
+        res.append(nums[i])
+        i += 1
+
+    while i + n <= len(nums) - 1:
+        res.append(nums[i + n])
+        i += 1
+
     return res
 
 
-shuffle([2, 5, 1, 3, 4, 7], 3)
+# print(shuffle([2, 5, 1, 3, 4, 7], 3))
+print(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4))
+# print(shuffle([1, 1, 2, 2], 2))
