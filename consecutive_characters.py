35c172c9e3d1e2c6c3fc6342c98a0bcf01d2b635
duy.leduc8 <duy.leduc8@gmail.com>
2021-12-25 17:17:37 +0700

    211225

---
+++ consecutive_characters.py
@@ -0,0 +1,30 @@
+def maxPower(s: str) -> int:
+    if s is None:
+        return -1
+
+    if len(s) == 0:
+        return 0
+    elif len(s) == 1:
+        return 1
+
+    prev = ''
+    cur = s[0]
+    powers = []
+    cur_len = 1
+
+    for c in s[1:]:
+        prev = cur
+        cur = c
+        if prev == cur:
+            cur_len += 1
+        else:
+            powers.append(cur_len)
+            cur_len = 1
+
+    powers.append(cur_len)
+
+    return max(powers)
+
+
+print(maxPower("leetcode"))
+print(maxPower("abbcccddddeeeeedcba"))
